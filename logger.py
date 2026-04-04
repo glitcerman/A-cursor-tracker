8856f08826c47e144e574fbf2c4d5d1ed4aefc2e
GlitcerMan <liambouf3@gmail.com>
2026-04-04 15:30:53 +0200

    Add files via upload

--- logger.py
+++ logger.py
@@ -2,22 +2,16 @@ import os, datetime
 
 os.makedirs("logs", exist_ok=True)
 
-if not os.path.exists("logs\\log_n.txt"):
-    log_n = 0
-    with open("logs\\log_n.txt", "w") as f:
-        f.write(str(log_n))
-else:
-    with open("logs\\log_n.txt", "r") as f:
-        content = f.read().strip()
-        log_n = int(content) if content else 0
-    with open("logs\\log_n.txt", "w") as f:
-        f.write(str(log_n + 1))
+log_n = 0
+for file in os.listdir("logs"):
+    if file.startswith("log_") and file.endswith(".txt") and file != "log_n.txt":
+        log_n += 1
 
 def logging(message: str):
     global log_n
-    with open(f"logs\\log_{log_n}.txt", "a") as log_file:  # "a" au lieu de "w+"
+    with open(f"logs/log_{log_n}.txt", "a") as log_file:
         log_file.write(f"{datetime.datetime.now()} : {message}\n")
 
 def open_log(log_n=log_n):
-    with open(f"logs\\log_{log_n}.txt", "r") as log_file:
+    with open(f"logs/log_{log_n}.txt", "r") as log_file:
         return log_file.read()
