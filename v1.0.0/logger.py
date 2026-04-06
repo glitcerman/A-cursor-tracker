e16c70d0f8ef70134dc675a52e3acca7c41c266c
GlitcerMan <liambouf3@gmail.com>
2026-04-06 17:20:22 +0200

    Add files via upload

---
+++ v1.0.0/logger.py
@@ -0,0 +1,17 @@
+import os, datetime
+
+os.makedirs("logs", exist_ok=True)
+
+log_n = 0
+for file in os.listdir("logs"):
+    if file.startswith("log_") and file.endswith(".txt"):
+        log_n += 1
+
+def logging(message: str):
+    global log_n
+    with open(f"logs/log_{log_n}.txt", "a") as log_file:
+        log_file.write(f"{datetime.datetime.now()} : {message}\n")
+
+def open_log(log_n=log_n):
+    with open(f"logs/log_{log_n}.txt", "r") as log_file:
+        return log_file.read()
